95f749e800fa9617cf24d5e3835043850287d562
RealRoMong <kdu7368@gmail.com>
2021-07-17 00:30:21 +0900

    update

--- Problem_1/ColorSliderEasyQuiz/ColorSliderQuiz/ViewController.swift
+++ Problem_1/ColorSliderEasyQuiz/ColorSliderQuiz/ViewController.swift
@@ -20,5 +20,19 @@ class ViewController: UIViewController {
         
         BackGround.backgroundColor = UIColor(red: rvalue, green: 0, blue: 0, alpha: 1)
     }
+    @IBAction func greenSliderMoved(_ sender: UISlider) {
+        
+        let gvalue : CGFloat = CGFloat(sender.value / 255.0)
+        
+        BackGround.backgroundColor = UIColor(red: 0, green: gvalue, blue: 0, alpha: 1)
+        
+    }
+    @IBAction func blueSliderMoved(_ sender: UISlider) {
+        let bvalue : CGFloat = CGFloat(sender.value / 255.0)
+        
+        BackGround.backgroundColor = UIColor(red: 0, green: 0, blue: bvalue, alpha: 1)
+    }
+    
+    
 }
 
